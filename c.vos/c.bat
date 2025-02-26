4b5003bbafe3af47fefc6b70c76edb674fbdb549
Vitya1230 <168643889+Vitya1230@users.noreply.github.com>
2025-02-27 02:01:53 +0300

    Update c.bat

--- c.vos/c.bat
+++ c.vos/c.bat
@@ -2,13 +2,28 @@
 chcp 65001
 
 :main_menu
+cls
 echo Welcome Casino in VOS!
 echo.
 echo 1. Start
 echo 2. Exit
-
+echo.
 set /p var=Enter a number: 
 if %var%==1 goto start
 if %var%==2 exit
 
 :start
+cls
+echo Choose a language:
+echo.
+echo 1. English
+echo 2. Russian(Русский)
+echo.
+set /p var=Enter a number: 
+if %var%==1 goto en
+if %var%==2 goto ru
+
+:en
+
+
+:ru
